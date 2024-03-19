17533c5556bb3a060974e8bc043d6c108bef6284
g4briel-rp <leirbagrp@gmail.com>
2024-03-19 10:14:15 -0300

    dia 2

--- parser.py
+++ parser.py
@@ -7,6 +7,11 @@ def main():
     return args.filename
 
 if __name__ == '__main__':
+
+    caracteres = ['+', '-', '*', '/', '(', ')', ';', ',', '.']
+    caracteresEspeciais = ['=', '<', '>', ':']
+    duplas = ['<=', '>=', ':=', '==', '<>']    
+
     operadores_A = ['+', '-', '*', '/', 'mod', 'div']
     operadores_LRA = ['or', 'and', 'not', '==', '<>', '>', '>=', '<', '<=', ':=']
     palavras_reservadas = ['program', 'var', 'integer', 'real', 'string', 'begin', 'end', 'for', 'to', 'while', 'do', 'break', 'continue', 'if', 'else', 'then', 'read', 'write']
@@ -19,6 +24,7 @@ if __name__ == '__main__':
         'simbolos': simbolos
     }
 
+    numeroLinha = 1
     tokens = []
     aux = []
 
@@ -26,15 +32,80 @@ if __name__ == '__main__':
 
     for linha in arquivo:
         print(f"tamanho da linha: {len(linha)}")
-        for i in range(0, len(linha)):
-            print(f"letra: {linha[i]}")
-            if linha[i] != ' ' and linha[i] != '\n' and linha[i] != '\t' and linha[i] != '\r':
-                aux.append(linha[i])
-            else:
-                if aux != []:
-                    aux_str = ''.join(aux)
-                    tokens.append(aux_str)
+        print(f"linha: {linha}")
+
+        if len(linha) == 1:
+            continue
+
+        # verifica se o primeiro caractere é uma letra ou espaço (REGRA)
+        if linha[0].isalpha():
+            aux.append(linha[0])
+            for i in range(1, len(linha)):
+                if linha[i].isalnum():
+                    aux.append(linha[i])
+                elif linha[i] in caracteres:
+                    tokens.append(''.join(aux))               
                     aux = []
+                    aux.append(linha[i])
+                    tokens.append(''.join(aux))
+                    aux = []
+                elif linha[i] in caracteresEspeciais:
+                    tokens.append(''.join(aux))
+                    aux = []
+                    if linha[i:i+1] in duplas:
+                        aux.append(linha[i:i+1])
+                        tokens.append(''.join(aux))
+                    else:
+                        aux.append(linha[i])
+                        tokens.append(''.join(aux))
+                    aux = []
+                elif linha[i].isspace():
+                    if len(aux) > 0:
+                        tokens.append(''.join(aux))
+                        aux = []
+        elif linha[0].isspace():
+            # encontrar a primeira letra
+            j = 1
+            while linha[j].isspace() and j < len(linha) - 1:
+                j += 1
+            
+            if linha[j].isalpha():
+                aux.append(linha[j])
+                for i in range(j + 1, len(linha)):
+                    if linha[i].isalnum():
+                        aux.append(linha[i])
+                    elif linha[i] in caracteres:
+                        tokens.append(''.join(aux))               
+                        aux = []
+                        aux.append(linha[i])
+                        tokens.append(''.join(aux))
+                        aux = []
+                    elif linha[i] in caracteresEspeciais:
+                        tokens.append(''.join(aux))
+                        aux = []
+                        if linha[i:i+1] in duplas:
+                            aux.append(linha[i:i+1])
+                            tokens.append(''.join(aux))
+                        else:
+                            aux.append(linha[i])
+                            tokens.append(''.join(aux))
+                        aux = []
+                    elif linha[i].isspace():
+                        if len(aux) > 0:
+                            tokens.append(''.join(aux))
+                            aux = []
+        elif linha[0:1] == '//':
+            # comentário vai até o final da linha
+            continue
+        elif linha[0] == '{':
+            # encontrar o fechamento do comentário
+            continue
+        elif linha[0].isdigit() or linha[0] in tipos['operadores_A'] or linha[0] in tipos['operadores_LRA'] or linha[0] in tipos['simbolos']:
+            # retornar erro
+            print(f"Erro na linha: {numeroLinha}, coluna: 0")
+            break
+        
+        numeroLinha += 1
     
     print(tokens)
 

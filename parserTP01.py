a72bbded795f0fdc6d98a5b3d7cbd7b1665261e5
Gabriel DESKTOP <leirbagrp@gmail.com>
2024-03-27 15:08:15 -0300

    att

--- parserTP01.py
+++ parserTP01.py
@@ -6,15 +6,23 @@ def main():
     args = parser.parse_args()
     return args.filename
 
+def classifica(simbolos, var, value):
+    for s in simbolos.keys():
+        if simbolos[s] == var:
+            value = s
+
+    if value == None and isInt(var):
+        value = 42
+    elif value == None and isFloat(var):
+        value = 43
+    elif value == None and isVariavel(var):
+        value = 41
+
+    return value
+
 def printLexemas(lexemas):
     for t in lexemas:
         print(t)
-
-def encontraKey(simbolos, value):
-    for chave, val in simbolos.items():
-        if val == value:
-            return chave
-    return None
 
 def isVariavel(aux):
     return aux.isalnum()
@@ -31,6 +39,11 @@ def isValid(aux):
             if aux[i].isalpha():
                 return False
     return True
+
+def erroLexema(numeroLinha, i, aux, var):
+    print(
+        f"Erro na linha: {numeroLinha}, coluna: {(i - len(aux) + 1)}\nLexema inválido: {var}")
+    exit()
 
 if __name__ == '__main__':
 
@@ -81,53 +94,6 @@ if __name__ == '__main__':
         40: ')',
     }
 
-    tipos_tokens = {
-        'tkn_add': 1,
-        'tkn_sub': 2,
-        'tkn_mult': 3,
-        'tkn_divisao': 4,
-        'tkn_mod': 5,
-        'tkn_div': 6,
-        'tkn_or': 7,
-        'tkn_and': 8,
-        'tkn_not': 9,
-        'tkn_igual': 10,
-        'tkn_dif': 11,
-        'tkn_maior': 12,
-        'tkn_maior_igual': 13,
-        'tkn_menor': 14,
-        'tkn_menor_igual': 15,
-        'tkn_atrib': 16,
-        'tkn_program': 17,
-        'tkn_var': 18,
-        'tkn_integer': 19,
-        'tkn_real': 20,
-        'tkn_string': 21,
-        'tkn_begin': 22,
-        'tkn_end': 23,
-        'tkn_for': 24,
-        'tkn_to': 25,
-        'tkn_while': 26,
-        'tkn_do': 27,
-        'tkn_break': 28,
-        'tkn_continue': 29,
-        'tkn_if': 30,
-        'tkn_else': 31,
-        'tkn_then': 32,
-        'tkn_read': 33,
-        'tkn_write': 34,
-        'tkn_ponto_virgula': 35,
-        'tkn_virgula': 36,
-        'tkn_ponto': 37,
-        'tkn_dois_pontos': 38,
-        'tkn_abre_parenteses': 39,
-        'tkn_fecha_parenteses': 40,
-        'tkn_variavel': 41,
-        'tkn_numero_inteiro': 42,
-        'tkn_numero_real': 43,
-        'tkn_string': 44,
-    }
-
     numeroLinha = 1
     lexemas = []
     aux = []
@@ -135,13 +101,9 @@ if __name__ == '__main__':
     arquivo = open(main(), 'r')
 
     for linha in arquivo:
-        # print(f"tamanho da linha: {len(linha)}")
-        # print(f"linha: {linha}")
-
         if len(linha) == 1:
             numeroLinha += 1
             continue
-
         i = 0
         while i < len(linha):
             if linha[i].isalnum():
@@ -149,26 +111,13 @@ if __name__ == '__main__':
             elif linha[i].isspace():
                 if len(aux) > 0:
                     var = ''.join(aux)
-
                     if isValid(var):
                         value = None
-                        for s in simbolos.keys():
-                            if simbolos[s] == var:
-                                value = s
-
-                        if value == None and isInt(var):
-                            value = 42
-                        elif value == None and isFloat(var):
-                            value = 43
-                        elif value == None and isVariavel(var):
-                            value = 41
-
-                        lexemas.append((value, var, numeroLinha, i - len(aux)))
+                        value = classifica(simbolos, var, value)
+                        lexemas.append((value, var, numeroLinha, (i - len(aux) + 1)))
                         aux = []
                     else:
-                        print(
-                            f"Erro na linha: {numeroLinha}, coluna: {i - len(aux)}\nLexema inválido: {var}")
-                        exit()
+                       erroLexema(numeroLinha, i, aux, var)
             elif linha[i] in caracteres:
                 if linha[i:i+2] == '//':
                     linha = arquivo.readline()
@@ -178,107 +127,49 @@ if __name__ == '__main__':
                 else:
                     if len(aux) > 0:
                         var = ''.join(aux)
-
                         if isValid(var):
                             value = None
-                            for s in simbolos.keys():
-                                if simbolos[s] == var:
-                                    value = s
-
-                            if value == None and isInt(var):
-                                value = 42
-                            elif value == None and isFloat(var):
-                                value = 43
-                            elif value == None and isVariavel(var):
-                                value = 41
-
-                            lexemas.append((value, var, numeroLinha, i - len(aux)))
+                            value = classifica(simbolos, var, value)
+                            lexemas.append((value, var, numeroLinha, (i - len(aux) + 1)))
                             aux = []
                         else:
-                            print(
-                                f"Erro na linha: {numeroLinha}, coluna: {i - len(aux)}\nLexema inválido: {var}")
-                            exit()
+                            erroLexema(numeroLinha, i, aux, var)
                     aux.append(linha[i])
                     var = ''.join(aux)
                     value = None
-                    for s in simbolos.keys():
-                        if simbolos[s] == var:
-                            value = s
-
-                    if value == None and isInt(var):
-                        value = 42
-                    elif value == None and isFloat(var):
-                        value = 43
-                    elif value == None and isVariavel(var):
-                        value = 41
-
-                    lexemas.append((value, var, numeroLinha, i))
+                    value = classifica(simbolos, var, value)
+                    lexemas.append((value, var, numeroLinha, i + 1))
                     aux = []
             elif linha[i] in caracteresEspeciais:
                 if len(aux) > 0:
                     var = ''.join(aux)
-
                     if isValid(var):
                         value = None
-                        for s in simbolos.keys():
-                            if simbolos[s] == var:
-                                value = s
-
-                        if value == None and isInt(var):
-                            value = 42
-                        elif value == None and isFloat(var):
-                            value = 43
-                        elif value == None and isVariavel(var):
-                            value = 41
-
-                        lexemas.append((value, var, numeroLinha, i - len(aux)))
+                        value = classifica(simbolos, var, value)
+                        lexemas.append((value, var, numeroLinha, (i - len(aux) + 1)))
                         aux = []
                     else:
-                        print(
-                            f"Erro na linha: {numeroLinha}, coluna: {i - len(aux)}\nLexema inválido: {var}")
-                        exit()
-
+                       erroLexema(numeroLinha, i, aux, var)
                 d = (''.join(linha[i:i+2]))
                 if d in duplas:
                     aux.append(linha[i:i+2])
                     var = ''.join(aux)
                     value = None
-                    for s in simbolos.keys():
-                        if simbolos[s] == var:
-                            value = s
-
-                    if value == None and isInt(var):
-                        value = 42
-                    elif value == None and isFloat(var):
-                        value = 43
-                    elif value == None and isVariavel(var):
-                        value = 41
-
-                    lexemas.append((value, var, numeroLinha, i))
+                    value = classifica(simbolos, var, value)
+                    lexemas.append((value, var, numeroLinha, i + 1))
                     i += 1
                 else:
                     aux.append(linha[i])
                     var = ''.join(aux)
                     value = None
-                    for s in simbolos.keys():
-                        if simbolos[s] == var:
-                            value = s
-
-                    if value == None and isInt(var):
-                        value = 42
-                    elif value == None and isFloat(var):
-                        value = 43
-                    elif value == None and isVariavel(var):
-                        value = 41
-
-                    lexemas.append((value, var, numeroLinha, i))
+                    value = classifica(simbolos, var, value)
+                    lexemas.append((value, var, numeroLinha, i + 1))
                 aux = []
             elif linha[i] == '"':
                 k = i + 1
                 while (linha[k] != '"'):
                     aux.append(linha[k])
                     k += 1
-
                     if k == (len(linha) - 1):
                         linha = arquivo.readline()
                         numeroLinha += 1
@@ -290,10 +181,9 @@ if __name__ == '__main__':
                 i = k
                 var = ''.join(aux)
                 value = 44
-                lexemas.append((value, var, numeroLinha, i - len(aux)))
+                lexemas.append((value, var, numeroLinha, (i - len(aux) + 1)))
                 aux = []
             elif linha[i] == '{':
-
                 k = i + 1
                 while (linha[k] != '}'):
                     k += 1
@@ -312,5 +202,4 @@ if __name__ == '__main__':
                 exit()
             i += 1
         numeroLinha += 1
-
     printLexemas(lexemas)
